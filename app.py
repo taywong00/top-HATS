e9bcff118950159543586dbfbd6cb66d63cacdb9
Samantha Ngo <sngo1@stuy.edu>
2018-01-02 12:19:11 -0500

    app.py

--- app.py
+++ app.py
@@ -1,39 +1,25 @@
-# Team CYES
-# Samantha Ngo, Carol Pan, Eugene Thomas, Yuyang Zhang
+# Team top_HATS
+# Holden Higgins, Adam Abbas, Taylor Wong, Samantha Ngo
 # SoftDev1 pd7
-# P01 -- ArRESTed Development
-# 2017-11-16
+# P0# -- 
+# 2018-01-02
 
 from flask import Flask, render_template, request
 from flask import redirect, flash, url_for
-from util import api_calls as api
+from util import
 
 app = Flask (__name__)
 
-#home route, takes zipcode and other inputs
+# HOMEPAGE: brief description and two buttons--"Login" or "Create an account"
 @app.route("/")
-def hello_world():
-    print "**DIAG: has api key been acquired?**"
+def homepage():
+    return render_template("home.html");
 
-    '''will figure out how to display keys at later date'''
-    return render_template("base.html")
-
-#reach out to apis and acquire info
-@app.route("/info")
-def display_info():
-    #retrieving the name
-    name=request.args["name"]
-    #retrieving weather data
-    zipcode=request.args["zipcode"]
-    #code for api call have been moved to util/api_calls.py
-    wform = api.weathercall(zipcode)
-    #retrieving book data
-    age=request.args["age"]
-    #code for api call have been moved to util/api_calls.py
-    bform = api.bookcall(age)
-
-    return render_template("info.html", name=name, bookdata=bform, weatherdata=wform)
-
+# LOGIN: name of product/logo and then "Username:", "Password:", and "Don't have an account? <CREATE hyperlink> one."
+@app.route("/login")
+def login():
+    return render_template("login.html");
+   
 if __name__ == "__main__":
     app.debug = True
     app.run()
