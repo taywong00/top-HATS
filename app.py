2d38308dde44996e7f973cf6538f8f390a77da15
sngo1 <sngo1@stuy.edu>
2018-01-18 13:26:51 -0500

    Rebooting forms

--- app.py
+++ app.py
@@ -15,9 +15,10 @@ app.secret_key = os.urandom(32)
 
 # HOMEPAGE: brief description and two buttons--"Login" or "Create an account"
 @app.route("/")
-def homepage():
+def home():
     return render_template("home.html")
 
+# Status: Incomplete
 # LOGIN: name of product/logo and then "Username:", "Password:", and "Don't have an account? <CREATE hyperlink> one."
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -32,14 +33,22 @@ def login():
     else:
         return render_template('login.html')
 
+@app.route("/signup_page")
+def signup_page():
+	if session.get("username"):
+		return "Please log out before creating a new account."
+	else:
+		return render_template('signup.html')
+		
+# Status: Incomplete
 # LOGIN: name of product/logo and then "Username:", "Password:", and "Don't have an account? <CREATE hyperlink> one."
 @app.route("/create_account", methods=['GET', 'POST'])
 def create_account():
     # If the user is already logged in:
-    if session.get('username'):
-        return redirect('home')
+    #if session.get('username'):                            # If the user is logged in, they should not be able to access this page in the first place.
+     #   return redirect('home')
     # If the user clicks Create Account
-    else:
+	print "INFORMATION: ", request.form.get("create_account")
         '''
         print "Username:", request.form.get("username")
         print "password:", request.form.get("password")
@@ -47,49 +56,58 @@ def create_account():
         print "medium:", request.form.get("medium")
         print "hard:", request.form.get("hard")
         '''
-        username = request.form.get("username")
-        password = request.form.get("password")
-        if request.form.get("hard") == 'on':
-            level = "hard"
-        elif request.form.get("medium") == 'on':
-            level = 'medium'
-        else:
-            level = 'easy'
+	username = request.form.get("username")
+	password = request.form.get("password")
+	if request.form.get("hard") == 'on':
+		level = "hard"
+	elif request.form.get("medium") == 'on':
+		level = 'medium'
+	else:
+		level = 'easy'
 
-        return redirect('/login')
-    #else:
-     #   return render_template("signup.html")
+	return redirect('/login')
+#else:
+ #   return render_template("signup.html")
 
+# Status: DONE
 @app.route("/how_to", methods=['GET', 'POST'])
 def how_to():
     return render_template("how_to.html")
+# -------------
 
+# Status: Incomplete
 @app.route("/account", methods=['GET', 'POST'])
 def account():
     return render_template("account.html")
 
+# Status: Incomplete
 @app.route("/feed")
 def feed():
     articles = API_funcs.get_headlines("business")
     print articles
     return render_template("feed.html", headline=articles[0], headlinet=articles[1], headlineth=articles[2])
 
+# Status: Incomplete
 @app.route("/stats")
 def stats():
     return render_template("stats.html")
 
+# Status: Incomplete
 @app.route("/leaderboard")
 def leaderboard():
     return render_template("leaderboard.html")
 
+# Status: Incomplete
 @app.route("/transaction")
 def transaction():
     return render_template("transaction.html")
 
+# Status: Incomplete
 @app.route("/confirmation")
 def confirmation():
     return render_template("stats.html", message="Congratulations! Your transaction was successful! You're one step closer to becoming rich!", good=True)
 
+# Status: Incomplete
 @app.route("/logout",methods=['POST','GET'])
 def logout():
     if 'username' not in session:
