52cb304bc427b524c29f955ce426a478f5bceaae
Adam Abbas <aabbas@stuy.edu>
2018-01-24 00:49:26 -0500

    sync

--- app.py
+++ app.py
@@ -98,9 +98,7 @@ def account():
         stocks = data_builder.get_holdings(transactions.get_id(user))
         print stocks
         for stock in stocks:
-            print stock
             stock.append(transactions.getStockPrice(stock[0]))
-            print stock
         # Get User Balance
         # balance =
         # #moneyz = transactions.get_balance(user)
